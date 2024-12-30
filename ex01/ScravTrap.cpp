4749bd3915035a8ed4b962e4ebf8bd484c3876e4
Pablo Abad <pabad-ap@c2r19s6.42madrid.com>
2024-12-30 20:54:15 +0100

    Ex00 DONE, ex01 starting

--- ex01/ScravTrap.cpp
+++ ex01/ScravTrap.cpp
@@ -1,45 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*   ScravTrap.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabad-ap <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/30 18:58:24 by pabad-ap          #+#    #+#             */
-/*   Updated: 2024/12/30 19:47:18 by pabad-ap         ###   ########.fr       */
+/*   Created: 2024/12/30 20:26:06 by pabad-ap          #+#    #+#             */
+/*   Updated: 2024/12/30 20:26:28 by pabad-ap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
-#include "ClapTrap.hpp"
+#include "ScravTrap.hpp"
 
-ClapTrap::ClapTrap(void): _name("Undefined"), _hit_points(10), \
+ScravTrap::ScravTrap(void): _name("Undefined"), _hit_points(10), \
 	_energy(10), _attack_damage(0)
 
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-ClapTrap::ClapTrap( std::string name ): _name(name), _hit_points(10), \
+ScravTrap::ScravTrap( std::string name ): _name(name), _hit_points(10), \
 	_energy(10), _attack_damage(0)
 {
 	std::cout << "Named constructor called" << std::endl;
 }
 
-ClapTrap::ClapTrap( ClapTrap const &src ): _name(src.getName()), \
+ScravTrap::ScravTrap( ScravTrap const &src ): _name(src.getName()), \
 	_hit_points(src.getHitPoints()), _energy(src.getEnergy()), \
 	_attack_damage(src.getAttackDamage())
 {
 	std::cout << "Copy constructor called" << std::endl;
 }
 
-ClapTrap::~ClapTrap(void)
+ScravTrap::~ScravTrap(void)
 {
 	std::cout << "Default destructor called" << std::endl;
 }
 
-ClapTrap &ClapTrap::operator=( ClapTrap const &src)
+ScravTrap &ScravTrap::operator=( ScravTrap const &src)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &src)
@@ -52,19 +52,19 @@ ClapTrap &ClapTrap::operator=( ClapTrap const &src)
 	return ( *this );
 }
 
-void	ClapTrap::attack( const std::string& target )
+void	ScravTrap::attack( const std::string& target )
 {
 	if ( this->_hit_points == 0 )
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " can't attack, no hit points available" 
 			<< std::endl;
 	else if ( this->_energy == 0 )
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " can't attack, no energy available" 
 			<< std::endl;
 	else
 	{
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " attack " << target << ", causing " 
 			<< this->_attack_damage << " points of damage!"
 			<< std::endl;
@@ -72,7 +72,7 @@ void	ClapTrap::attack( const std::string& target )
 	}
 }
 
-void	ClapTrap::takeDamage( unsigned int amount )
+void	ScravTrap::takeDamage( unsigned int amount )
 {
 	if (this->_hit_points == 0)
 		std::cout << this->_name 
@@ -94,20 +94,20 @@ void	ClapTrap::takeDamage( unsigned int amount )
 	}
 }
 
-void 	ClapTrap::beRepaired( unsigned int amount)
+void 	ScravTrap::beRepaired( unsigned int amount)
 {
 	if ( this->_hit_points == 0 )
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " can't be repaired, no hit points available" 
 			<< std::endl;
 	else if ( this->_energy == 0 )
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " can't be repaired, no energy available" 
 			<< std::endl;
 	else
 	{
 		this->_hit_points += amount;
-		std::cout << "ClapTrap " << this->_name \
+		std::cout << "ScravTrap " << this->_name \
 			<< " repaired " << amount << " hit points. " 
 			<< "Current hit point: " << this->_hit_points
 			<< std::endl;
@@ -115,42 +115,42 @@ void 	ClapTrap::beRepaired( unsigned int amount)
 	}
 }
 
-std::string	ClapTrap::getName(void) const
+std::string	ScravTrap::getName(void) const
 {
 	return( this->_name );
 }
 
-int		ClapTrap::getHitPoints(void) const
+int		ScravTrap::getHitPoints(void) const
 {
 	return( this->_hit_points );
 }
 
-int		ClapTrap::getEnergy(void) const
+int		ScravTrap::getEnergy(void) const
 {
 	return( this->_energy );
 }
 
-int		ClapTrap::getAttackDamage(void) const
+int		ScravTrap::getAttackDamage(void) const
 {
 	return( this->_attack_damage );
 }
 
-void		ClapTrap::setName( std::string name )
+void		ScravTrap::setName( std::string name )
 {
 	this->_name = name;
 }
 
-void		ClapTrap::setHitPoints( int amount )
+void		ScravTrap::setHitPoints( int amount )
 {
 	this->_hit_points = amount;
 }
 
-void		ClapTrap::setEnergy( int amount )
+void		ScravTrap::setEnergy( int amount )
 {
 	this->_energy = amount;
 }
 
-void		ClapTrap::setAttackDamage( int amount )
+void		ScravTrap::setAttackDamage( int amount )
 {
 	this->_attack_damage = amount;
 }
---
+++ ex01/ScravTrap.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScravTrap.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabad-ap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 20:26:36 by pabad-ap          #+#    #+#             */
+/*   Updated: 2024/12/30 20:31:20 by pabad-ap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCRAVTRAP_CLASS_HPP
+# define SCRAVTRAP_CLASS_HPP
+
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScravTrap: public ClapTrap
+{
+	public:
+		ScravTrap(void); //Default constructor
+		ScravTrap(std::string name);
+		ScravTrap( ScravTrap const & src ); //Copy constructor
+		~ScravTrap(void); //Default destructor
+
+		ScravTrap &operator=( ScravTrap const &src); //Copy assignment operator.
+		
+		void		attack( const std::string& target );
+};
+
+#endif
